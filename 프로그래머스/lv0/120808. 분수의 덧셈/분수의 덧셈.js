0b96208d5e34025788678e0187759b08b6d25714
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-03-11 18:33:53 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,13 +1,12 @@
+function fnGCD(a, b) {
+    return (a % b) ? fnGCD(b, a % b) : b;
+}
+
 function solution(numer1, denom1, numer2, denom2) {
     let numer = numer1 * denom2 + numer2 * denom1;
     let denom = denom1 * denom2;
     
-    let gcd;
-    for (var i = 1; i <= numer; i++) {
-        if (numer % i === 0 && denom % i === 0) {
-            gcd = i;
-        }
-    }
+    let gcd = fnGCD(numer, denom);
     
     let answer = [numer / gcd, denom / gcd];
     return answer;
