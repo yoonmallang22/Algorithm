5a591c06134974dcb7a2d5c7f1bc0c01856701db
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-03-24 15:34:37 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,6 +1,3 @@
 function solution(money) {
-    let answer = [];
-    answer.push(Math.floor(money/5500));
-    answer.push(money%5500)
-    return answer;
+    return [Math.floor(money/5500), money%5500];
 }
