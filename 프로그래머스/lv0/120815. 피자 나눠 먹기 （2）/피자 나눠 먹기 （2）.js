aca15b61c585a28995fd0d9dea74de12cdab8e55
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-03-25 11:58:59 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,15 +1,7 @@
 function solution(n) {
-    if (n % 6 === 0) {
-        return n / 6;
-    } else {
-        let gcd = 1;
-        let lcm = 1;
-        for (let i = 0; i <= 6; i++){
-            if ((n % i === 0) && (6 % i === 0)){
-                gcd = i;
-            }
-        }
-        lcm = n * 6 / gcd;
-        return lcm / 6;
+    let pizza = 1;
+    while (pizza * 6 % n){
+        pizza++;
     }
+    return pizza;
 }
