2a71e926caeb607cf8d80edb106c41eea6c9aa9a
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-03-30 22:58:58 +0900

    [level 0] Title: 순서쌍의 개수, Time: 39.32 ms, Memory: 50.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,10 +1,3 @@
 function solution(n) {
-    let answer = 0;
-    for (let i = 1; i < n+1; i++) {
-        if (n % i === 0) {
-            answer++;
-        }
-    }
-    
-    return answer;
+    return Array(n).fill(1).map((v,i) => v + i).filter(v => n % v === 0).length
 }
