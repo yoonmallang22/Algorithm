f213ec63ff401a84878e419423eb1a1524a293be
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-04-02 11:02:15 +0900

    [level 0] Title: 양꼬치, Time: 0.04 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,7 +1,4 @@
 function solution(n, k) {
-    if (n < 10) {
-        return 12000 * n + 2000 * k;
-    } else {
-        return 12000 * n + 2000 * k - 2000 * Math.floor(n/10);
-    }
+    k -= ~~(n/10);
+    return 12000 * n + 2000 * k;
 }
