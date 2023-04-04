8c0e43b796418ad7547b7b09cdedc7e9cc6d4f6d
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-04-04 11:59:25 +0900

    [level 0] Title: 모스부호 (1), Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -6,5 +6,5 @@ function solution(letter) {
     '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
     '-.--':'y','--..':'z'
     }
-    return letter.split(' ').map(v => morse[v]).join('');
+    return letter.split(' ').reduce((prev, curr) => prev + morse[curr], '')
 }
