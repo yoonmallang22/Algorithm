d43808ce77fa8abffdf203846051965775e0c117
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-04-07 01:17:27 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,11 +1,3 @@
 function solution(s1, s2) {
-    let num = 0;
-    for (const i in s1){
-        for (const j in s2){
-            if(s1[i] === s2[j]) {
-                num++;
-            }
-        }
-    }
-    return num;
+    return s1.filter(v => s2.includes(v)).length
 }
