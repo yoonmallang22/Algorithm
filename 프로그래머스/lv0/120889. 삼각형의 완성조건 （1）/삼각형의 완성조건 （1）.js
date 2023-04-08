29677625afae7a2d3a1a14977312cee5344b8849
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-04-08 22:29:13 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,8 +1,4 @@
 function solution(sides) {
     sides.sort((a, b) => a - b);
-    if (sides[0] + sides[1] > sides[2]) {
-        return 1;
-    } else {
-        return 2;
-    }
+    return (sides[0] + sides[1] > sides[2]) ? 1 : 2;
 }
