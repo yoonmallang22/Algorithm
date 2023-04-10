867ad60e1952971b4f6b7e0c7805453726a79efa
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-04-10 23:40:21 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,4 +1,4 @@
 function solution(box, n) {
-    const arr = box.map(v => ~~(v/n));
-    return arr[0]*arr[1]*arr[2];
+    const [width, depth, height] = box;
+    return ~~(width/n) * ~~(depth/n) * ~~(height/n);
 }
