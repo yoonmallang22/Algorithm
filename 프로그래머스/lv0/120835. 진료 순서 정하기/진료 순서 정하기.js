3727ce29c416d3df87ee43e25193721768bc185e
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-04-11 18:24:36 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,4 +1,4 @@
 function solution(emergency) {
-    const newEmergency = emergency.slice('').sort((a,b)=>b-a);
+    const newEmergency = [...emergency].sort((a,b)=>b-a);
     return emergency.map(v => newEmergency.indexOf(v)+1);
 }
