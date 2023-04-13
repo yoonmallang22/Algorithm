0b7d9fafc2c9aa80c7b42d3ca880d58bd407ad16
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-04-14 00:38:10 +0900

    [level 0] Title: 암호 해독, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,7 +1,7 @@
 function solution(cipher, code) {
-    let arr = [];
-    for (let i = 1; i <= cipher.length; i++) {
-        arr.push([...cipher][code * i - 1]);
+    let arr = '';
+    for (let i = code - 1; i < cipher.length; i += code) {
+        arr += cipher[i];
     }
-    return arr.join('');
+    return arr;
 }
