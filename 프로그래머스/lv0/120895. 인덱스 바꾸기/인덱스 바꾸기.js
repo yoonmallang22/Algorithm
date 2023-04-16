f203a2b94ed016b62f0cbf347e0bd4d3e08961ed
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-04-16 18:15:27 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,6 +1,5 @@
 function solution(my_string, num1, num2) {
     let arr = [...my_string];
-    arr.splice(num1, 1, my_string[num2]);
-    arr.splice(num2, 1, my_string[num1]);
+    [arr[num1], arr[num2]] = [arr[num2], arr[num1]];
     return arr.join('');
 }
