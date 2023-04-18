03ce643563b28cbaddb4cd77bdbff32350cc5001
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-04-18 12:10:15 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,5 +1,3 @@
 function solution(my_string) {
-    return [...my_string].map(v => parseInt(v))
-        .filter(v => !Number.isNaN(v))
-        .sort((a, b) => a- b);
+    return my_string.match(/[0-9]/g).map(v => +v).sort((a, b) => a-b);
 }
