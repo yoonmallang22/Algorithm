1f04bc4dde2dddcd2cb76af5197d33651a0deda0
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-04-20 19:26:58 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,6 +1,6 @@
 function solution(numbers) {
     let result;
     let num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    num.forEach((v, i) => numbers = numbers.replaceAll(v, i));
+    num.forEach((v, i) => numbers = numbers.split(v).join(i));
     return parseInt(numbers);
 }
