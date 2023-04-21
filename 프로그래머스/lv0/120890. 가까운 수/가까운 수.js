847dd2fcd83f55cd74e3c8a07521336c5f685a00
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-04-21 13:27:30 +0900

    [level 0] Title: 가까운 수, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,6 +1,5 @@
 function solution(array, n) {
-    const newArr = array.sort((a, b) => a - b).map(v => Math.abs(v-n));
-    const min = Math.min(...newArr);
-    const idx = newArr.indexOf(min);
-    return array[idx];
+    array.sort((a,b) => Math.abs(n - a) - Math.abs(n - b) || a - b);
+
+    return array[0];
 }
