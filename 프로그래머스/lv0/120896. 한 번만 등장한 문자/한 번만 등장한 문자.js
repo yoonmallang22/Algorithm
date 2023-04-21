460384d5bcbe4192f9a0621c16def9226a318330
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-04-21 13:59:15 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,16 +1,3 @@
 function solution(s) {
-    const arr = [...new Set(s)].sort();
-    let countArr = Array(arr.length).fill(0);
-    for(let i of [...s]) {
-        for(let j = 0; j < arr.length; j++) {
-            if(i === arr[j]) countArr[j]++;
-        }
-    }
-    
-    let str = '';
-    countArr.forEach((v, i) => {
-        if(v === 1) str += arr[i];
-    });
-    
-    return str;
+    return [...s].filter(v => s.match(new RegExp(v,'g')).length == 1).sort().join('');
 }
