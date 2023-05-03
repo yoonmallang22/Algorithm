b9535fd6634249ab4431f4595b629aafaf3246bf
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-05-03 19:25:00 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,12 +1,3 @@
 function solution(spell, dic) {
-    return dic.map(v => [...new Set(v)].join(''))
-        .filter(v => {
-        let cnt = 0;
-        for (item of spell) {
-            if(v.includes(item)) {
-                cnt += 1;
-            }
-        }
-        return cnt === spell.length
-    }).length > 0 ? 1 : 2
+    return dic.some(s => spell.sort().toString() == [...s].sort().toString()) ? 1 : 2;
 }
