3604b0ee55bef924db71bff407bed9ab1e495a12
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-05-06 18:58:24 +0900

    [level 0] Title: OX퀴즈, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,10 +1,10 @@
 function solution(quiz) {
     let answer = [];
-    return quiz.map(t => {
-        const [calc, result] = t.split(' = ');
-        const sign = calc.includes('+') ? 1 : -1
-        const [a, b] = calc.split(sign === 1 ? ' + ' : ' - ');
-
-        return +a + (+b * sign) === +result ? 'O' : 'X'
-    });
+    for(let x of quiz){
+        const splitArr = x.split(' ');
+        const result = splitArr.includes('-') ? parseInt(splitArr[0]) - parseInt(splitArr[2]) : parseInt(splitArr[0]) + parseInt(splitArr[2]);
+        
+        answer.push(parseInt(splitArr[splitArr.length-1]) === result ? "O" : "X")
+    }
+    return answer;
 }
