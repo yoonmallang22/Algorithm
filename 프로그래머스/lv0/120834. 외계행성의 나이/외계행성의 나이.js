523493504c7f83c2e0ed02e965d4f27cfb15d8fc
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-05-13 01:24:39 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,3 +1,10 @@
 function solution(age) {
-  return age.toString().split('').map((v) => "abcdefghij"[v]).join('');
+    let answer = '';
+    let newAge = age.toString();
+    let str = 'abcdefghijklmnopqrstuvwxyz';
+    for(let i = 0; i < newAge.length; i++){
+        let num = newAge[i];
+        answer += str[num];
+    }
+    return answer;
 }
