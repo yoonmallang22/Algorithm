b90a32ca83530a63f2c1024f28e32f3461a8140c
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-05-13 23:33:00 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,14 +1,8 @@
 function solution(my_string) {
-    const alp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let arr = new Array(52).fill(0);
     
-    [...my_string].filter(v => {
-        for (item of alp) {
-            if (v === item){
-                arr[alp.indexOf(item)]++;
-            }
-        }
-    })
+    [...my_string].map(v => arr[alp.indexOf(v)]++);
     
     return arr;
 }
