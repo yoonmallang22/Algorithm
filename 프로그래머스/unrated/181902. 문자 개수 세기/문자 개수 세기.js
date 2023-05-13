b0886023b1ca186eed0c0f727d1e8908525a8aa1
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-05-13 23:28:26 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.19 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,14 @@
+function solution(my_string) {
+    const alp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    
+    [...my_string].filter(v => {
+        for (item of alp) {
+            if (v === item){
+                arr[alp.indexOf(item)]++;
+            }
+        }
+    })
+    
+    return arr;
+}
