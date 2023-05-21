1ec8ae3b5e1b7553116bde8527ac11ab00f62e5c
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-05-21 10:19:24 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,10 +1,10 @@
 function solution(a, b) {
-    let g = 1;
     for (let i = 1; i <= b; i++) {
-        if (a % i === 0 && b % i === 0)  g = i
+        if (a % i === 0 && b % i === 0) {
+            a = a / i;
+            b = b / i;
+        }
     }
-    
-    b = b / g;
     
     while (b % 2 === 0) b = b / 2;
     while (b % 5 === 0) b = b / 5;
