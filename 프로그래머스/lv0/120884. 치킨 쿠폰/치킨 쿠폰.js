bf40028daccf8855eb2ec13c57275dea52eb459f
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-05-26 18:06:31 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,10 @@
+function solution(chicken) {
+    let result = 0;
+    
+    while(chicken >= 10){
+    	result += Math.floor(chicken / 10);
+        chicken = chicken % 10 + Math.floor(chicken / 10);
+    }
+    
+    return result;
+}
