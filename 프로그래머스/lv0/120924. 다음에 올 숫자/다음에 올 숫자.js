ab6a6416ca857a6e397ffa93ab8979e3b3407fc7
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-05-28 23:44:40 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,8 +1,8 @@
 function solution(common) {
     if (common[1] - common[0] === common[2] - common[1]) {
-        return common.pop() + common[1] - common[0];
+        return common[common.length - 1] + common[1] - common[0];
     }
     else {
-        return common.pop() * common[1] / common[0];
+        return common[common.length - 1] * common[1] / common[0];
     }
 }
