991a193f0e99c06c41fa6b57fd9a5fdd60c7852f
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-05-30 18:29:53 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,10 +1,3 @@
 function solution(A, B) {
-    if (A === B) return 0;
-    
-    for (let i = 0; i < A.length; i++) {
-        A = A.slice(-1) + A.slice(0,-1);
-        if (A === B) return i+1;
-    }
-    
-    return -1;
+    return (B+B).indexOf(A);
 }
