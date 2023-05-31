591d4718b79b9ada4efd37c5d4f99358f8683661
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-05-31 20:17:09 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
@@ -1,3 +1,8 @@
 function solution(n, k) {
-    return Array(n).fill(1).map((v, i) => v + i).filter(v => v % k === 0);
+    let answer = [];
+    for(let i = k ; i <= n; i+=k){
+        answer.push(i);
+    }
+    
+    return answer;
 }
