963c244f18f030f71f65cc7b81f64bbcc7e3cd47
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-06-06 08:18:23 +0900

    [level 0] Title: 옹알이 (1), Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,20 +1,10 @@
 function solution(babbling) {
     let answer = 0;
-    let can = ["aya", "ye", "woo", "ma"];
-    
-    for (const i in babbling) {
-        let init = babbling[i];
-        
-        for (const j in can) {
-            if (babbling[i].includes(can[j])) {
-                init = init.replace(can[j], "");
-            }
-        }
-        
-        if (init.length === 0) {
-            answer += 1;
-        }
-    }
+    const regex = /^(aya|ye|woo|ma)+$/;
+
+    babbling.forEach(word => {
+      if (regex.test(word)) answer++;  
+    })
     
     return answer;
 }
