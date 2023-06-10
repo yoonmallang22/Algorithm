db3d1a46271ec15f10ce0cc24f4313a4a279e4e1
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-06-11 08:21:01 +0900

    [unrated] Title: 카운트 업, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
@@ -1,8 +1,3 @@
 function solution(start, end) {
-    let answer = [];
-    for (let i = start; i < end + 1; i++) {
-        answer.push(i);
-    }
-    
-    return answer;
+    return new Array(end - start + 1).fill(0).map((_, i) => start + i);
 }
