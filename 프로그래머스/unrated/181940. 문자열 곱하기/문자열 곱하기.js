8aae1609f074ca977e5d05366c962d4352e6b403
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-07-02 07:33:51 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,7 +1,3 @@
 function solution(my_string, k) {
-    let answer = '';
-    for (let i = 0; i < k; i++) {
-        answer += my_string;
-    }
-    return answer;
+    return my_string.repeat(k);
 }
