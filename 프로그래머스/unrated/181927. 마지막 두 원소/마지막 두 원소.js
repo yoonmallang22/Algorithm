c1f6b939cac4effba9c19230a9816f197c4af031
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-07-15 21:04:59 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,5 +1,8 @@
 function solution(num_list) {
-    num_list[num_list.length - 1] > num_list[num_list.length - 2] ? num_list[num_list.length] = num_list[num_list.length - 1] - num_list[num_list.length - 2] : num_list[num_list.length] = num_list[num_list.length - 1] * 2;
-    
-    return num_list;
+    return [
+        ...num_list,
+        num_list[num_list.length - 1] > num_list[num_list.length - 2] ?
+            num_list[num_list.length - 1] - num_list[num_list.length - 2] :
+            num_list[num_list.length - 1] * 2
+    ]
 }
