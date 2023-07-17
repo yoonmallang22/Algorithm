93a1b45d283d9b71661940e85d8f10c4b89000d1
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-07-17 14:59:08 +0900

    [unrated] Title: 글자 지우기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -1,8 +1,6 @@
 function solution(my_string, indices) {
     let arr = my_string.split('');
-    for (let i = 0; i < indices.length; i++) {
-        arr.splice(indices[i], 1, ' ');
-    }
+    indices.map(v => arr.splice(v, 1, ' '));
     
     return arr.join('').replaceAll(' ', '');
 }
