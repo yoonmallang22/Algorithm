dfd37675cfef93a72b6e46ad16df597926dd11fd
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-08-05 00:17:36 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,9 +1,3 @@
 function solution(numbers, n) {
-    let answer = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        answer += numbers[i];
-        if (answer > n) break;
-    }
-    
-    return answer;
+    return numbers.reduce((acc, cur) => acc > n ? acc : acc + cur, 0);
 }
