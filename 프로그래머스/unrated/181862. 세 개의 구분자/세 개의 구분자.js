64d12d03c6456de18d5dc548b4c2411f39a9c6d9
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-08-27 09:34:26 +0900

    [unrated] Title: 세 개의 구분자, Time: 70.94 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,3 +1,5 @@
 function solution(myStr) {
-    return myStr.split('a').map(v => v.split('b')).flat().map(v => v.split('c')).flat().filter(v => v != "").length !== 0 ? myStr.split('a').map(v => v.split('b')).flat().map(v => v.split('c')).flat().filter(v => v != "") : ["EMPTY"]
+    const arr = myStr.split('a').map(v => v.split('b')).flat().map(v => v.split('c')).flat().filter(v => v != "");
+    
+    return arr.length ? arr : ["EMPTY"];
 }
