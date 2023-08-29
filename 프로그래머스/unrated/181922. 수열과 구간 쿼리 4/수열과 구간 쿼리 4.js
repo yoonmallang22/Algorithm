87a5085705f6d1b171f8459fc535fe4851298f90
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-08-29 14:21:12 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 188.91 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,7 +1,8 @@
 function solution(arr, queries) {
     for (let i = 0; i < queries.length; i++) {
-        for (let j = queries[i][0]; j <= queries[i][1]; j++) {
-            if (j % queries[i][2] === 0) arr[j]++;
+        const [s, e, k] = queries[i];
+        for (let j = s ; j <= e ; j++) {
+            if (j % k === 0) arr[j]++;
         }
     }
     
