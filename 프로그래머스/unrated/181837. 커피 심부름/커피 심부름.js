acb4b5047295e705c356c7cfad0eb0c9255c93ff
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-09-04 10:10:37 +0900

    [unrated] Title: 커피 심부름, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -1,10 +1,3 @@
 function solution(order) {
-    let money = 0;
-    order.map(v => {
-        if (v.includes('americano')) money += 4500;
-        else if (v.includes('cafelatte')) money += 5000;
-        else money += 4500;
-    });
-    
-    return money;
+    return order.reduce((acc, cur) => acc + (cur.includes('latte') ? 5000 : 4500), 0);
 }
