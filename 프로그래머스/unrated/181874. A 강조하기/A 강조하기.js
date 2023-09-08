9ec4d9f2b2e920066a7fc3ee78b88665013f38d4
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-09-08 23:08:31 +0900

    [unrated] Title: A 강조하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -1,3 +1,3 @@
 function solution(myString) {
-    return [...myString].map(v => (v === 'a' || v === 'A') ? v.toUpperCase() : v.toLowerCase()).join('');
+    return myString.toLowerCase().replaceAll("a","A");
 }
