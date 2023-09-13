2ab7d36756603f9e8b95226fef5d405eb0903f03
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-09-13 21:34:53 +0900

    [unrated] Title: 0 떼기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
@@ -1,9 +1,3 @@
 function solution(n_str) {
-    let idx = 0;
-    for (let i = 0; i < n_str.length; i++) {
-        if (n_str[i] !== '0') break;
-        idx++;
-    }
-    
-    return n_str.slice(idx);
+    return +n_str + '';
 }
