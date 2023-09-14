b08cb1bd5022743123f70a75b09a05fd8615a87b
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-09-14 10:44:41 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,13 +1,3 @@
 function solution(arr, idx) {
-    let answer;
-    for (let i = idx; i < arr.length; i++) {
-        if (arr[i] === 1) {
-            answer = i;
-            break;
-        } else {
-            answer = -1;
-        }
-    }
-    
-    return answer;
+    return arr.indexOf(1, idx);
 }
