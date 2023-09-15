6ac0d3a2d8add5dd5106c921144dccc1237db397
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-09-15 21:25:15 +0900

    [unrated] Title: 그림 확대, Time: 0.09 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.js
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.js
@@ -1,11 +1,5 @@
 function solution(picture, k) {
-    return picture.map(v => [...v].map(v => {
-        let str = '';
-        for(let i = 0; i < k; i++) {
-            str += v;
-        }
-        return str;
-    }).join('')).map(v => {
+    return picture.map(v => [...v].map(v => v.repeat(k)).join('')).map(v => {
         let arr = [];
         for(let i = 0; i < k; i++) {
             arr.push(v);
