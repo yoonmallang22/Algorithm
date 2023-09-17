41fd6a3d310e24a38982a77358e395073780946e
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-09-17 13:53:53 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 1.87 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -1,15 +1,5 @@
 function solution(arr, k) {
-    let result = [];
-    
-    arr.map(v => {
-        if (result.indexOf(v) === -1) result.push(v);
-    });
-    
-    if (result.length < k) {
-        for (i = result.length; i < k; i++) result[i] = -1;
-    } else if (result.length > k) {
-        result.length = k;
-    }
-    
-    return result;
+    const set = new Set(arr);
+  
+    return set.size < k ? [...set, ...Array(k - set.size).fill(-1)] : [...set].slice(0, k);
 }
