d884ba274c900bd9e735e4203fed27f8ff029202
yoonmallang22 <126536548+yoonmallang22@users.noreply.github.com>
2023-09-20 07:33:52 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,9 +1,15 @@
 function solution(a, b, c) {
-    if (a !== b && b !== c && c !== a) {
-        return a + b + c;
-    } else if (a === b && b === c) {
-        return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3);
-    } else {
-        return (a + b + c) * (a**2 + b**2 + c**2);
+    const cnt = new Set([a, b, c]).size;
+
+    switch(cnt) {
+        case 3: 
+            return a + b + c;
+            break;
+        case 2: 
+            return (a + b + c) * (a ** 2 + b ** 2 + c ** 2);
+            break;
+        case 1: 
+            return (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3);
+            break;
     }
 }
