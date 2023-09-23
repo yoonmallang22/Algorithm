b5adfdc3274fb0a75149a2aef8f90ce819e0f614
JISOO YOON <yoonmallang@gmail.com>
2023-09-23 22:33:32 +0900

    [unrated] Title: 대소문자 바꿔서 출력하기, Time: 74.15 ms, Memory: 32.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    
+    let answer = '';
+    for (let i of str) {
+        const alp = i === i.toUpperCase() ? i.toLowerCase() : i.toUpperCase();
+        answer += alp;
+    }
+    
+    console.log(answer);
+});
