5d8d66088cfdad35d8cc850f19e603d4612a6c84
JISOO <yoonmallang@gmail.com>
2023-09-26 00:19:24 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 12.26 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,21 +1,3 @@
 function solution(arr, queries) {
-    let result = [];
-    
-    for (let [s, e, k] of queries) {
-        let newArr = [];
-        
-        for (let i = s; i <= e; i++) {
-            if (k < arr[i]) {
-                newArr.push(arr[i]);
-            }
-        }
-        
-        if (newArr.length === 0) {
-            result.push(-1);
-        } else {
-            result.push(Math.min(...newArr));
-        }
-    }
-    
-    return result;
+    return queries.map(([s, e, k]) => arr.slice(s, e + 1).filter(n => n > k).sort((a, b) => a - b)[0] || -1);
 }
