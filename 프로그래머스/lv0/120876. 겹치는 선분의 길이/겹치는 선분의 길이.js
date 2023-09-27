abeaa7a40b49da4c1ab80a9ed12c63c3cca27d8a
JISOO <yoonmallang@gmail.com>
2023-09-27 16:02:02 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,16 +1,9 @@
 function solution(lines) {
-    const min = Math.min(...lines.flat());
-    lines = lines.map(([start, end]) => [start - min, end - min]);
-    const arr = new Array(3).fill().map(_ => new Array(Math.max(...lines.flat())).fill(0));
-    lines.map(([s,e], idx) => {
-        for(let i = s; i < e; i++) arr[idx][i] = 1
+    let line = new Array(200).fill(0);
+
+    lines.forEach(([a, b]) => {
+        for(; a < b; a++) line[a + 100]++;
     });
-    
-    let answer = 0;
-    
-    for(let i = 0; i < Math.max(...lines.flat()); i++) {
-        answer += (arr[0][i] + arr[1][i] + arr[2][i]) >= 2;
-    }
-    
-    return answer;
+
+    return line.reduce((a, c) => c > 1 ? a + 1 : a, 0);
 }
