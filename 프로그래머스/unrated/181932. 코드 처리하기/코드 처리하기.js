b04afa7e3d6a05dd8261e9f542001c7c179d54e9
JISOO <yoonmallang@gmail.com>
2023-09-30 09:14:22 +0900

    [unrated] Title: 코드 처리하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -2,15 +2,14 @@ function solution(code) {
     let ret = '';
     let mode = 0;
     
-    [...code].map((v, idx) => {
-        if (v === "1") {
-            mode = mode ? 0 : 1;
-        } else {
-            if (idx % 2 === mode) {
-                ret += v;
-            }
-        }
-    });
+    for (let i = 0; i < code.length; i += 1) {
+      if (code[i] === "1") {
+        mode = mode ? 0 : 1;
+      }
+      if (code[i] !== "1" && i % 2 === mode) {
+        ret += code[i];
+      }
+    }
     
     return ret ? ret : "EMPTY";
 }
