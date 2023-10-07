6c4fe85da119e236082c1d17eded9a3877d3d35b
JISOO <yoonmallang@gmail.com>
2023-10-07 20:38:12 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,11 +1,7 @@
 function solution(n) {
-    let result;
     for (let i = 0; i < n; i++) {
         if (n % i === 1) {
-            result = i;
-            break;
+            return i;
         }
     }
-    
-    return result;
 }
