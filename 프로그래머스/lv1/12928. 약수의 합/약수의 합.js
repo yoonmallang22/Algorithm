79da723a41060b2bbd9a42bd0f659235923e0765
JISOO <yoonmallang@gmail.com>
2023-10-07 11:57:30 +0900

    [level 1] Title: 약수의 합, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    let arr = [];
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) arr.push(i);
+    }
+    
+    return arr.reduce((acc, cur) => acc + cur, 0);
+}
