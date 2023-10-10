e2221c3f06b638d525951257e906547ba703d072
JISOO <yoonmallang@gmail.com>
2023-10-10 15:35:40 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.35 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,23 +1,3 @@
 function solution(n, arr1, arr2) {
-    let entireMap = new Array(n).fill('');
-    
-    arr1.map(v => v.toString(2).padStart(n, '0')).map((val, idx) => {
-        for (let i = 0; i < val.length; i++) {
-            if (val[i] === '1') {
-                entireMap[idx] += '#';
-            } else {
-                entireMap[idx] += ' ';
-            }
-        }
-    });
-    
-    arr2.map(v => v.toString(2).padStart(n, '0')).map((val, idx) => {
-        for (let i = 0; i < val.length; i++) {
-            if (val[i] === '1') {
-                entireMap[idx] = entireMap[idx].slice(0, i) + '#' + entireMap[idx].slice(i + 1);
-            }
-        }
-    });
-    
-    return entireMap;
+    return Array(n).fill().map((_, i) => (arr1[i] | arr2[i]).toString(2).padStart(n, '0').replace(/0|1/g, v => +v ? '#' : ' '));
 }
