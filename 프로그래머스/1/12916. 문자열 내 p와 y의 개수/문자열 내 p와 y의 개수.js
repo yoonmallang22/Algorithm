3aa682a6273f49cfd2663b31ec766d52c9eaa364
JISOO <yoonmallang@gmail.com>
2023-10-11 22:56:32 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,12 +1,3 @@
 function solution(s){
-    let p = 0;
-    let y = 0;
-    
-    s.toLowerCase().split('').forEach(v => {
-        if (v === "p") p++;
-        else if (v === "y") y++;
-    })
-    
-    if (p === y) return true;
-    else return false;
+    return s.toLowerCase().split("p").length === s.toLowerCase().split("y").length;
 }
