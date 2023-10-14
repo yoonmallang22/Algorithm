dd4b41dba9674fb77714f716a0688a65ed7acad5
JISOO <yoonmallang@gmail.com>
2023-10-14 09:43:00 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    let answer = 0;
+    
+    if (a > b) [a, b] = [b, a];
+    
+    for (let i = a; i <= b; i++) {
+        answer += i;
+    }
+    
+    return answer;
+}
