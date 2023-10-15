83be090bc0f7f466ebfa41bad527b85bbe522655
JISOO <yoonmallang@gmail.com>
2023-10-15 21:35:31 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,19 @@
+function solution(num) {
+    let result = 0;
+    
+    if (num === 1) return 0;
+    
+    while (num !== 1) {
+        if (num % 2) {
+            num = num * 3 + 1;
+        } else {
+            num = num / 2;
+        }
+        
+        result++;
+        
+        if (result > 500) return -1;
+    }
+    
+    return result;
+}
