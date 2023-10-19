ac2dcbb8eb2d1a0d0da34ae1a7ddffb8bafcbd35
JISOO <yoonmallang@gmail.com>
2023-10-19 13:52:39 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,6 +1,5 @@
 function solution(arr) {
-    const a = [...arr].sort((a, b) => b - a).pop();
-    arr.splice(arr.indexOf(a), 1);
+    arr.splice(arr.indexOf(Math.min(...arr)), 1);
     
     return arr.length ? arr : [-1];
 }
