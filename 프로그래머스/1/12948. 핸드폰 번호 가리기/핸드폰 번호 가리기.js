039ddf25543678a6ccfc4a2b2beae481bba3fd9e
JISOO <yoonmallang@gmail.com>
2023-10-19 13:40:02 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,8 +1,3 @@
 function solution(phone_number) {
-    let result = '';
-    for (let i = 0; i < phone_number.length - 4; i++) {
-        result += '*';
-    }
-    
-    return result + phone_number.slice(phone_number.length - 4);
+    return '*'.repeat(phone_number.length - 4) + phone_number.slice(-4);
 }
