f6691565436e966caa3bcb6001cc21c0f58babf9
JISOO <yoonmallang@gmail.com>
2023-10-21 18:52:41 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,20 @@
+function solution(left, right) {
+    let result = 0;
+    for (let i = left; i <= right; i++) {
+        let count = 0;
+        
+        for (let j = 1; j <= i; j++) {
+            if (i % j === 0) {
+                count += 1;
+            }
+        }
+        
+        if (count % 2 === 0) {
+            result += i;
+        } else {
+            result -= i;
+        }
+    }
+    
+    return result;
+}
