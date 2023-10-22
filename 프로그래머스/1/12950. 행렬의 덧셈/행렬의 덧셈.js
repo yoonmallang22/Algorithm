c0eba41cd555b8980e323a045db72fac573378b2
JISOO <yoonmallang@gmail.com>
2023-10-22 21:54:55 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.72 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,9 +1,3 @@
 function solution(arr1, arr2) {
-    for (let i = 0; i < arr1.length; i++) {
-        for (let j = 0; j < arr1[i].length; j++) {
-            arr1[i][j] = arr1[i][j] + arr2[i][j];
-        }
-    }
-    
-    return arr1;
+    return arr1.map((v, i) => v.map((v2, i2) => v2 + arr2[i][i2]));
 }
