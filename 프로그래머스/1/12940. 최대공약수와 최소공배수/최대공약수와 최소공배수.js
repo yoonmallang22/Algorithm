302219d2de2e00922061fe91cca061fc9fa717e5
JISOO <yoonmallang@gmail.com>
2023-10-23 17:36:35 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 4.87 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,19 @@
+function solution(n, m) {
+    let gcd = 1;
+    let lcm = 1;
+    
+    for (let i = 2; i <= Math.min(n, m); i++) {
+        if (n % i === 0 && m % i === 0) {
+            gcd = i;
+        }
+    }
+
+    while (true) {
+      if ((lcm % n === 0) && (lcm % m === 0)){
+        break;
+      }
+      lcm++;
+    }
+    
+    return [gcd, lcm];
+}
