6cde82497a76576b07b4e09097825c2418307b72
JISOO <yoonmallang@gmail.com>
2023-10-26 22:45:11 +0900

    [level 1] Title: 예산, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,21 +1,11 @@
 function solution(d, budget) {
-    let result;
-    try {
-        d.sort((a,b) => a-b).forEach((v, i) => {
-            budget -= v;
-            if (budget < 0) {
-                result = i;
-                throw new Error("stop loop");
-            } else if (budget === 0) {
-                result = i + 1;
-                throw new Error("stop loop");
-            } else if (i + 1 === d.length) {
-                result = i + 1;
-            }
-        });
-    } catch (e) {
-        
-    }
+    let result = 0;
+    d.sort((a,b) => a-b).forEach((v, i) => {
+        budget -= v;
+        if (budget >= 0) {
+            result++;
+        } 
+    });
     
     return result;
 }
