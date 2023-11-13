e0a79cecc135e43146438ecd36736cd42fe00017
JISOO <yoonmallang@gmail.com>
2023-11-13 15:41:51 +0900

    [level 1] Title: 2016년, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,5 +1,13 @@
 function solution(a, b) {
-    const day = String(new Date(2016, a-1, b)).slice(0, 3);
+    const dayList = ['FRI','SAT','SUN','MON','TUE','WED','THU'];
+    const monthlyDayArr = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let daySum;
     
-    return day.toUpperCase();
+    if (a < 2) {
+        daySum = b - 1;
+    } else {
+        daySum = monthlyDayArr.slice(0, a-1).reduce((a, b) => a + b, 0) + b - 1;
+    }
+    
+    return dayList[daySum % 7];
 }
