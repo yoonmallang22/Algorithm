d9b1e7f1246fff443b1bb6e44c68051f9c2a4ab0
JISOO <yoonmallang@gmail.com>
2023-11-23 05:54:56 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.46 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -0,0 +1,35 @@
+function solution(numbers, hand) {
+    const position = {
+        1: [0, 3], 2: [1, 3], 3: [2, 3],
+        4: [0, 2], 5: [1, 2], 6: [2, 2],
+        7: [0, 1], 8: [1, 1], 9: [2, 1],
+        '*': [0, 0], 0: [1, 0], '#': [2, 0]
+    };
+    const handed = {
+        left: 'L', right: 'R'
+    };
+    
+    let leftHand = position['*'];
+    let rightHand = position['#'];
+    
+    return numbers.map(v => {
+        if (v === 1 || v === 4 || v === 7) {
+            leftHand = position[v];
+            return 'L';
+        } else if (v === 3 || v === 6 || v === 9) {
+            rightHand = position[v];
+            return 'R';
+        } else {
+            if (Math.abs(leftHand[0] - position[v][0]) + Math.abs(leftHand[1] - position[v][1]) < Math.abs(rightHand[0] - position[v][0]) + Math.abs(rightHand[1] - position[v][1])) {
+                leftHand = position[v];
+                return 'L';
+            } else if (Math.abs(leftHand[0] - position[v][0]) + Math.abs(leftHand[1] - position[v][1]) > Math.abs(rightHand[0] - position[v][0]) + Math.abs(rightHand[1] - position[v][1])) {
+                rightHand = position[v];
+                return 'R';
+            } else {
+                hand === 'left' ? leftHand = position[v] : rightHand = position[v];
+                return handed[hand];
+            }
+        }
+    }).join('');
+}
