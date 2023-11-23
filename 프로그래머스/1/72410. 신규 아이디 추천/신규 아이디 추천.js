52771c3e45385812d82ccce2a21b4a41f1588386
JISOO <yoonmallang@gmail.com>
2023-11-23 14:50:27 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,7 +1,5 @@
 function solution(new_id) {
-    new_id = new_id.toLowerCase().replace(/[^a-z0-9\-\_\.]/g, '').replace(/\.+/g, '.').replace(/^\./g, '').replace(/\.$/g, '');
-    
-    new_id = new_id.length === 0 ? new_id += 'a' : new_id;
+    new_id = new_id.toLowerCase().replace(/[^a-z0-9\-\_\.]/g, '').replace(/\.+/g, '.').replace(/^\.|\.$/g, '').replace(/^$/g, 'a');
     
     if (new_id.length >= 16) return new_id.slice(0, 15).replace(/\.$/g, '');
     if (new_id.length <= 2) return new_id + new_id[new_id.length-1].repeat(3 - new_id.length);   
