230cd6c1004737b9d0cadd21b03c5a518a234f02
JISOO <yoonmallang@gmail.com>
2023-11-23 13:24:58 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.59 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,12 +1,14 @@
 function solution(survey, choices) {
-    let result = '';
-    const characterScore = {
-        R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0
-    };
-    
-    survey.forEach((v, i) => {
-        characterScore[v[choices[i] > 4 ? 1 : 0]] += Math.abs(choices[i] - 4);
+    const typesScore = {};
+    const types = ["RT","CF","JM","AN"];
+
+    types.forEach((type) => type.split('').forEach((char) => typesScore[char] = 0));
+
+    choices.forEach((choice, index) => {
+        const [disagree, agree] = survey[index];
+
+        typesScore[choice > 4 ? agree : disagree] += Math.abs(choice - 4);
     });
-    
-    return `${characterScore.R >= characterScore.T ? 'R' : 'T'}${characterScore.C >= characterScore.F ? 'C' : 'F'}${characterScore.J >= characterScore.M ? 'J' : 'M'}${characterScore.A >= characterScore.N ? 'A' : 'N'}`;
+
+    return types.map(([a, b]) => typesScore[b] > typesScore[a] ? b : a).join("");
 }
