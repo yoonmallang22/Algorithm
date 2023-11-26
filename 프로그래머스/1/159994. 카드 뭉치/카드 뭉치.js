be80d9883dba309198176052233dca1cb3b835bc
JISOO <yoonmallang@gmail.com>
2023-11-26 15:52:34 +0900

    [level 1] Title: 카드 뭉치, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,10 +1,13 @@
 function solution(cards1, cards2, goal) {
-    let arr = [];
-    
-    goal.forEach((v) => {
-        if(cards1[0] === v) arr.push(cards1.shift());
-        if(cards2[0] === v) arr.push(cards2.shift());
-    })
-    
-    return arr.toString() === goal.toString() ? 'Yes' : 'No';
+    for(const s of goal) {
+        if(cards1[0] === s) {
+            cards1.shift();
+        } else if(cards2[0] === s) {
+            cards2.shift();
+        } else {
+            return "No"
+        }
+    }
+
+    return "Yes";
 }
