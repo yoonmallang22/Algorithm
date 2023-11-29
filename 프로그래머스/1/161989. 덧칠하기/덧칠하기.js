14ad65969df6e9bc36b9df062de9bd319108a81b
JISOO <yoonmallang@gmail.com>
2023-11-29 22:39:54 +0900

    [level 1] Title: 덧칠하기, Time: 6.26 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,11 +1,15 @@
 function solution(n, m, section) {
     let cnt = 0;
-    let part = 0;
+    const arr = Array(n).fill(0);
     
+    section = section.map(v => v - 1);
     section.forEach(v => {
-        if (v > part) {
-          part = v + m - 1;
-          cnt++;
+        arr[v] = 1;
+    });    
+    section.forEach(v => {
+        if (arr[v]) {
+            arr.fill(0, v, v+m);
+            cnt++;
         }
     });
     
