a1bb0e9eafb4e884ef662f6ff7f64a7362221fe0
JISOO <yoonmallang@gmail.com>
2023-12-04 19:21:38 +0900

    [level 1] Title: 옹알이 (2), Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -3,7 +3,7 @@ function solution(babbling) {
     return babbling.reduce((total, current) => {
         let babble = current;
         for (let i = 0; i < can.length; i++) {
-            if (current.includes(can[i].repeat(2))) break;
+            if (current.includes(can[i].repeat(2))) return total;
             babble = babble.split(can[i]).join(' ');
         }
         return total + (babble.split(' ').join('').length === 0);
