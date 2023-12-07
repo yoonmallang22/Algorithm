f65770b676d99d61c392e0e28f420ed6c7a47155
JISOO <yoonmallang@gmail.com>
2023-12-07 19:59:55 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    let result = '';
+    s = s.toLowerCase();
+    
+    for (let i = 0; i < s.length; i++) {
+        if (i === 0 || s[i-1] === ' ') result += s[i].toUpperCase();
+        else result += s[i];
+    }
+    
+    return result;
+}
