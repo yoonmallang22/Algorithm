ad17e9137e41e90177872086d1a4071f5c2c97a6
JISOO <yoonmallang@gmail.com>
2023-12-09 21:22:16 +0900

    [level 2] Title: 올바른 괄호, Time: 9.97 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,13 +1,10 @@
 function solution(s){
-    if (s.length % 2 !== 0) return false;
-    if (s[0] === ')' || s[s.length-1] === '(') return false;
-    if (s.split('').filter(v => v === '(').length !== s.length / 2) return false;
-    let left = 0;
-    let right = 0;
-    for (let i = 0; i < s.length; i++) {
-        s[i] === '(' ? left++ : right++;
-        if (left < right) return false;
+    let cnt = 0;
+    
+    for (let char of s) {
+        cnt += char === '('? 1 : -1;
+        if (cnt < 0) return false;
     }
     
-    return true;
+    return cnt === 0 ? true : false;
 }
