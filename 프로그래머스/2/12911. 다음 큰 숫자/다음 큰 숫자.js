4daf458cec2b5fea9cf7a7cb139a6093dc4cf89a
JISOO <yoonmallang@gmail.com>
2023-12-13 05:00:40 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.08 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,12 +1,14 @@
 function solution(n) {
     const cnt1 = [...n.toString(2)].filter(v => v === '1').length;
-    let cnt2 = 0;
     let num = n+1;
     
-    while (cnt1 !== cnt2) {
-        cnt2 = [...num.toString(2)].filter(v => v === '1').length;
+    while (n < num) {
+        const cnt2 = [...num.toString(2)].filter(v => v === '1').length;
+
+        if (cnt1 === cnt2) break;
+        
         num++;
     }
     
-    return num-1;
+    return num;
 }
