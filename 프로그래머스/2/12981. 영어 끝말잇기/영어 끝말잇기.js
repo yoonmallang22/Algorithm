8f8d6de1ec57158503847c7fc2afc384beb5adc8
JISOO <yoonmallang@gmail.com>
2023-12-18 21:43:24 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -4,7 +4,7 @@ function solution(n, words) {
 
     for (let i = 0; i < words.length; i++) {
         const last = game[game.length-1];
-        if (game.includes(words[i]) || (last && last[last.length-1] !== words[i][0])) {
+        if (game.includes(words[i]) || (last && last.substring(last.length-1) !== words[i].substring(0,1))) {
             result[0] = i%n + 1;
             result[1] = Math.floor(i/n) + 1; 
             break;
