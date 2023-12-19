0b990bf706214b6d28f8d54eda2e3242ffa78b48
JISOO <yoonmallang@gmail.com>
2023-12-19 23:58:27 +0900

    [level 2] Title: 숫자의 표현, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    let answer = 0;
+    for (let i = 1; i * (i - 1) / 2 < n; i++) {
+        if ((n - (i * (i - 1) / 2)) % i == 0) answer++;
+    }
+    
+    return answer;
+}
