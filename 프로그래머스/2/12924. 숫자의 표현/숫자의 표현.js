735763493c876d06ea0655a507979110ec5a1d79
JISOO <yoonmallang@gmail.com>
2023-12-20 10:50:12 +0900

    [level 2] Title: 숫자의 표현, Time: 0.37 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,17 +1,9 @@
 function solution(n) {
-    let result = 1;
-    for (let i = 1; i < n/2; i++) {
-        let sum = i;
-        for (let j = i+1; j < n; j++) {
-            sum += j;
-            if (sum > n) {
-                break;
-            }
-            if (sum === n) {
-                result++;
-                break;
-            }
-        }
+    // 주어진 자연수를 연속된 자연수의 합으로 표현하는 방법의 수와
+    // 주어진 수의 홀수인 약수 개수는 같다.
+    let result = 0;
+    for (i = 1; i <= n; i++) {
+        if (n % i === 0 && i % 2 === 1) result++;
     }
     
     return result;
