b539cede97633d2efd30514ca6acfa463f7391e4
JISOO <yoonmallang@gmail.com>
2023-12-27 11:22:28 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,22 +1,16 @@
 function solution(n) {
-    let cur = 1;
-    let move = 1;
-    let result = 1;
+    let result = 0;
     
-    while (cur < n) {
-        if (cur + move === n) {
-            break;
-        }
-        
-        if (cur + move < n) {
-            cur += move;
-            move *= 2;
-        } else {
+    // 도착지점에서 거꾸로 되감기
+    // 이동한 지점이 홀수일 경우 점프를, 짝수를 경우 순간 이동을 사용하여 이전 지점으로 되돌아감
+    while (n !== 0) {
+        if (n % 2 === 1) {
             result++;
-            move = 1;
-            cur += move;
+            n -= 1;
+        } else {
+            n /= 2;
         }
     }
-    
+
     return result;
 }
