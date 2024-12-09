df9e32b9b90d8a91ae0b664c13f9ab74ab6157c5
JISOO <yoonmallang@gmail.com>
2024-12-09 18:18:39 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 3.77 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,24 +1,22 @@
 function solution(board, moves) {
-    let result = 0;
-    let basket = [];
-    moves = moves.map(v => v-1);
+    const stack = [];
+    let cnt = 0;
     
     for (let i = 0; i < moves.length; i++) {
         for (let j = 0; j < board.length; j++) {
-            if (board[j][moves[i]] !== 0) {
-                if (basket[basket.length - 1] === board[j][moves[i]]) {
-                    basket.pop();
-                    result += 2;
+            if (board[j][moves[i]-1] !== 0) {
+                if (stack.length > 0 && board[j][moves[i]-1] === stack[stack.length-1]) {
+                    stack.pop();
+                    cnt += 2;
+                } else {
+                    stack.push(board[j][moves[i]-1]);
                 }
-                else {
-                    basket.push(board[j][moves[i]]);
-                }
-                board[j][moves[i]] = 0;
+                
+                board[j][moves[i]-1] = 0;
                 break;
             }
         }
     }
-
-    return result;
-}
     
+    return cnt;
+}
