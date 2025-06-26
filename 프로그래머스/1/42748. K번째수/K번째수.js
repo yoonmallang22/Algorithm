44ccae6550bd96859b251d56eef3e7809e94abbf
JISOO <yoonmallang@gmail.com>
2025-06-26 15:58:47 +0900

    [level 1] Title: K번째수, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,3 +1,11 @@
 function solution(array, commands) {
-    return commands.map(([i, j, k]) => array.slice(i-1, j).sort((a, b) => a - b)[k-1]);
+    const arr = [];
+    
+    for (let l = 0; l < commands.length; l++) {
+        const [i, j, k] = commands[l];
+        const result = array.slice(i - 1, j).sort((a, b) => a - b)[k - 1];
+        arr.push(result);
+    }
+    
+    return arr;
 }
