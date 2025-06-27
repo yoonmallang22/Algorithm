83cc07db1cbb68b3cbc8228e1fe712d9e6c23a09
JISOO <yoonmallang@gmail.com>
2025-06-27 15:45:28 +0900

    [level 2] Title: H-Index, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,15 +1,3 @@
 function solution(citations) {
-    citations.sort((a, b) => b - a);
-    
-    let h = 0;
-    for (let i = 0; i < citations.length; i++) {        
-        // i번째 논문은 citations[i]번 인용됨
-        if (citations[i] >= i + 1) {
-            h = i + 1;
-        } else {
-            break;
-        }
-    }
-    
-    return h;
+    return citations.sort((a, b) => b - a).filter((v, i) => v >= i + 1).length;
 }
